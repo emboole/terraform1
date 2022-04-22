5a9f1f8ee08143cc588546c49f618dfb7ace3bb1
emboole <nicolas@plw.com.ar>
2022-04-22 12:17:51 -0300

    descomentado la hosted zone

--- internal_hosted_zone.tf
+++ internal_hosted_zone.tf
@@ -1,19 +1,19 @@
-# resource "aws_route53_zone" "private" {
-#   name = "testn.internal"
-#   tags = {
-#     Name = var.tags_hz.private_zone_name
-#   }
+resource "aws_route53_zone" "private" {
+  name = "testn.internal"
+  tags = {
+    Name = var.tags_hz.private_zone_name
+  }
 
-#   vpc {
-#     vpc_id = aws_vpc.testn.id
-#   }
-# }
+  vpc {
+    vpc_id = aws_vpc.testn.id
+  }
+}
 
-# resource "aws_vpc" "testn" {
-#   cidr_block       = "10.0.0.0/16"
-#   instance_tenancy = "default"
+resource "aws_vpc" "testn" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
 
-#   tags = {
-#     Name = var.tags_hz.vpc_name
-#   }
-# }
+  tags = {
+    Name = var.tags_hz.vpc_name
+  }
+}
