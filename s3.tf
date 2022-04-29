41c2dbd9f959be6968464ff8aae1a1f91a1619f4
root <nicolas@plw.com.ar>
2022-04-29 17:23:13 -0300

    asg y s4 con index

--- s3.tf
+++ s3.tf
@@ -20,23 +20,23 @@ resource "aws_s3_bucket" "bucketn" {
     }
 }
 
-resource "aws_s3_bucket" "bucket_asg" {
-    bucket = var.bucket_asg_name
-    acl    = "private"
+# resource "aws_s3_bucket" "bucket_asg" {
+#     bucket = var.bucket_asg_name
+#     acl    = "private"
 
-    # al pedo
-    # versioning {
-    #     enabled = true
-    # }
+#     # al pedo
+#     # versioning {
+#     #     enabled = true
+#     # }
 
-    tags = {
-        Name = "Pal ASG con un index.html"
-    }
-}
+#     tags = {
+#         Name = "Pal ASG con un index.html"
+#     }
+# }
 
-resource "aws_s3_bucket_object" "file_upload" {
-  bucket = "bucket_asg"
-  key    = "index.html"
-  source = "index.html"
-#   etag   = "${filemd5("${path.module}/my_files.zip")}"
-}
+# resource "aws_s3_bucket_object" "file_upload" {
+#   bucket = "bucket_asg"
+#   key    = "index.html"
+#   source = "index.html"
+# #   etag   = "${filemd5("${path.module}/my_files.zip")}"
+# }
