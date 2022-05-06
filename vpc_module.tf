7c1a64b92871dce01082efc126f68dc445e19b83
root <nicolas@plw.com.ar>
2022-05-06 20:10:37 -0300

    testn a cjove

--- vpc_module.tf
+++ vpc_module.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "testn"
+  name = "cjove"
   cidr = "10.0.0.0/16"
 
   azs             = [var.subnets_az.az_1, var.subnets_az.az_2]
