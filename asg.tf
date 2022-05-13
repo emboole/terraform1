b46024f7d0b694dd15ab17c15592f23c89679f78
emboole <nicolas@plw.com.ar>
2022-05-13 14:05:24 -0300

    plw

--- asg.tf
+++ asg.tf
@@ -1,5 +1,5 @@
-resource "aws_launch_template" "ec2_testn" {
-  name_prefix   = "ec2_testn"
+resource "aws_launch_template" "ec2_cjove" {
+  name_prefix   = "ec2_cjove"
   image_id      = "ami-0f9fc25dd2506cf6d"
   instance_type = "t2.micro"
   security_group_names = [aws_security_group.allow_80.name]
@@ -11,21 +11,21 @@ resource "aws_launch_template" "ec2_testn" {
     device_name = "/dev/sda1"
 
     ebs {
-      kms_key_id = aws_kms_key.testn_key.arn
+      kms_key_id = aws_kms_key.cjove_key.arn
     }
   }
 
   user_data = filebase64("bootstraps/bootstrap.sh")
 }
 
-resource "aws_autoscaling_group" "asg_testn" {
+resource "aws_autoscaling_group" "asg_cjove" {
   availability_zones = ["us-east-1a"]
   desired_capacity   = 1
   max_size           = 1
   min_size           = 1
 
   launch_template {
-    id      = aws_launch_template.ec2_testn.id
+    id      = aws_launch_template.ec2_cjove.id
     version = "$Latest"
   }
 }
@@ -57,7 +57,7 @@ resource "aws_security_group" "allow_80" {
   }
 }
 
-resource "aws_kms_key" "testn_key" {
+resource "aws_kms_key" "cjove_key" {
   description             = "KMS key 1"
   deletion_window_in_days = 10
 }
--- vpc_module.tf
+++ vpc_module.tf
@@ -1,18 +1,18 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "cjove"
+#   name = "cjove"
   cidr = "10.0.0.0/16"
 
   azs             = [var.subnets_az.az_1, var.subnets_az.az_2]
   private_subnets = ["10.0.0.0/24"]
   public_subnets  = ["10.0.1.0/24"]
 
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
+#   enable_nat_gateway = true
+#   enable_vpn_gateway = true
 
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
+#   tags = {
+#     Terraform = "true"
+#     Environment = "dev"
+#   }
 }
